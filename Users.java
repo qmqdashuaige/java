bcd96b78dbf2664e03a91c39bfa8195c49036b97
qmq <156555648@qq.com>
2017-01-21 17:20:16 +0800

    Signed-off-by: qmq <156555648@qq.com>

---
+++ Users.java
@@ -0,0 +1,26 @@
+package com.entity;
+
+/**
+ * Created by 15655 on 2017/1/20.
+ */
+public class Users {
+
+    private String username;
+
+    public Users(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public static void main(String[] str){
+    
+      System.out.println();
+
+  }
+}
